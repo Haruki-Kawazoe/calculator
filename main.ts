453262eb069963863e516998605dee264a1aa185
Haruki <75521368+Haruki-Kawazoe@users.noreply.github.com>
2022-06-12 14:33:07 +0900

    ブロックが見やすいように変更

--- main.ts
+++ main.ts
@@ -1,9 +1,50 @@
+function リセット () {
+    basic.showString("Reset")
+    Aの数 = 0
+    Bの数 = 0
+    和差積商 = 0
+    記号の数 = 0
+}
+function 計算 () {
+    if (記号の数 == 1) {
+        basic.showNumber(Aの数 + Bの数)
+        和差積商 = Aの数 + Bの数
+        Aの数 = 和差積商
+        Bの数 = 0
+    } else if (記号の数 == 2) {
+        basic.showNumber(Aの数 - Bの数)
+        和差積商 = Aの数 - Bの数
+        Aの数 = 和差積商
+        Bの数 = 0
+    } else if (記号の数 == 3) {
+        basic.showNumber(Aの数 * Bの数)
+        和差積商 = Aの数 * Bの数
+        Aの数 = 和差積商
+        Bの数 = 0
+    } else if (記号の数 == 4) {
+        basic.showNumber(Aの数 / Bの数)
+        和差積商 = Aの数 / Bの数
+        Aの数 = 和差積商
+        Bの数 = 0
+    }
+}
 input.onButtonPressed(Button.A, function () {
-    Aの数 += 1
-    basic.showNumber(Aの数)
+    ボタンAが押された時()
 })
 input.onPinPressed(TouchPin.P2, function () {
     記号の数 += 1
+    記号のLED()
+})
+input.onButtonPressed(Button.AB, function () {
+    リセット()
+})
+input.onButtonPressed(Button.B, function () {
+    ボタンBが押された時()
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    計算()
+})
+function 記号のLED () {
     if (記号の数 == 1) {
         basic.showLeds(`
             . . # . .
@@ -39,43 +80,17 @@ input.onPinPressed(TouchPin.P2, function () {
     } else if (記号の数 == 5) {
         記号の数 = 1
     }
-})
-input.onButtonPressed(Button.AB, function () {
-    basic.showString("Reset")
-    Aの数 = 0
-    Bの数 = 0
-    和差積商 = 0
-    記号の数 = 0
-})
-input.onButtonPressed(Button.B, function () {
+}
+function ボタンAが押された時 () {
+    Aの数 += 1
+    basic.showNumber(Aの数)
+}
+function ボタンBが押された時 () {
     Bの数 += 1
     basic.showNumber(Bの数)
-})
-input.onLogoEvent(TouchButtonEvent.Pressed, function () {
-    if (記号の数 == 1) {
-        basic.showNumber(Aの数 + Bの数)
-        和差積商 = Aの数 + Bの数
-        Aの数 = 和差積商
-        Bの数 = 0
-    } else if (記号の数 == 2) {
-        basic.showNumber(Aの数 - Bの数)
-        和差積商 = Aの数 - Bの数
-        Aの数 = 和差積商
-        Bの数 = 0
-    } else if (記号の数 == 3) {
-        basic.showNumber(Aの数 * Bの数)
-        和差積商 = Aの数 * Bの数
-        Aの数 = 和差積商
-        Bの数 = 0
-    } else if (記号の数 == 4) {
-        basic.showNumber(Aの数 / Bの数)
-        和差積商 = Aの数 / Bの数
-        Aの数 = 和差積商
-        Bの数 = 0
-    }
-})
+}
+let 記号の数 = 0
 let 和差積商 = 0
 let Bの数 = 0
-let 記号の数 = 0
 let Aの数 = 0
 basic.showString("Calculator ")
